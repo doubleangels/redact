9b49d80a73bd3230ec08be3ce74fa6e168934961
doubleangels <matt@doubleangels.com>
2026-06-05 13:22:44 -0600

    Clean up temp scripts

--- print_ex.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('app/src/main/java/com/doubleangels/redact/media/FormatConverter.java', 'r') as f:
-    content = f.read()
-
-content = content.replace('com.doubleangels.redact.sentry.SentryManager.recordException(e);', 'e.printStackTrace();\n            com.doubleangels.redact.sentry.SentryManager.recordException(e);')
-
-with open('app/src/main/java/com/doubleangels/redact/media/FormatConverter.java', 'w') as f:
-    f.write(content)
